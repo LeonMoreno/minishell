6c5ff7ff1b7f5bf90afcbc83075c3b33733094db
Leonardo Moreno <lmoreno@student.42quebec.com>
2022-06-01 19:43:30 -0400

    premier pas setenv

---
+++ premiers_pas/setenv_ens.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+extern char **environ;
+
+int main(int argc, char **arv, char **env)
+{
+	//printf("%s\n", environ[0]);
+	//*environ = malloc(sizeof(char) * 5);
+	if (setenv("LEOA", "100", 0) != 0)
+		perror("setenv");
+	//*environ = "leo=1";	
+
+	//printf("%s\n", *environ);
+	int i;
+	char **envs;
+	envs = env;
+	i = 0;
+	while (environ[i])
+	{
+		printf("%s\n", environ[i]);
+		i++;
+	}
+	return(0);
+}
+
