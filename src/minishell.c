900157983918c1a2c1ed7fb452b7ac35c2307ec5
Andre-paul Grenon <grenonap@gmail.com>
2022-06-03 15:58:22 -0400

    Minishell

--- src/minishell.c
+++ src/minishell.c
@@ -1,33 +1,18 @@
 #include "minishell.h"
 
-void	free_lst(t_tokens *top)
-{
-	t_tokens *begin;
-	t_tokens *temp;
-
-	begin = top;
-	while(begin)
-	{
-		temp = begin;
-		begin = begin->next;
-		free (temp);
-	}
-}
-
 void start_shell(t_sh *sh)
 {
 	while (1)
 	{
 		sh->line = readline("miniShell$ ");
 		if (sh->line && sh->line[0] != '\0')
-			line_parser(sh);	
-//		if (sh->token_top)
-//			check_builtins(sh);
+			line_parser(sh);		
+		if (sh->token_top)
+			check_builtins(sh);
 		if (sh->line)
 			add_history(sh->line);
-		//exec_cmd(sh);
-		free(sh->line);
-		free_lst(sh->token_top);
+		//exec_cmd(sh);		
+		free_lst(sh);
 		sh->token_top = NULL;
 	}
 }
