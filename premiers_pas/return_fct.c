6f0b429a83b90ad5732b14cfd83cb425e9589b06
Leonardo Moreno <leon.moreno@pm.me>
2022-06-02 21:32:08 -0400

    return fct

---
+++ premiers_pas/return_fct.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int ft_ft(void)
+{
+	int	x;
+
+	x = 7;
+	if (x == 7)
+		return (0);
+	else
+		return (1);
+	return (7);
+}
+
+int main(void)
+{
+	int re;
+
+	if (re = ft_ft())
+		printf("Return if %d\n", re);
+	printf("Return fuerra %d\n", re);
+	
+	return (0);
+}
